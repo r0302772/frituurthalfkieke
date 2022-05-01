7cf76bd9f2b120fa8777c8510175ebd28b592cd4
NR0893 <reniersniels@hotmail.com>
2022-05-01 11:03:14 +0200

    Bestelling controller/bestelling entity/sql in comment

---
+++ src/main/java/be/thomasmore/graduaten/frituurthalfkieke/controllers/ArtikelController.java
@@ -0,0 +1,15 @@
+/*package be.thomasmore.graduaten.frituurthalfkieke.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ArtikelController {
+    @RequestMapping("/artikel")
+    public String navigateToArtikel(){
+
+        return "artikel";
+    }
+
+}*/
---
+++ src/main/java/be/thomasmore/graduaten/frituurthalfkieke/controllers/BestellingController.java
@@ -0,0 +1,61 @@
+package be.thomasmore.graduaten.frituurthalfkieke.controllers;
+
+import be.thomasmore.graduaten.frituurthalfkieke.entities.Bestelling;
+import be.thomasmore.graduaten.frituurthalfkieke.entities.Student;
+import be.thomasmore.graduaten.frituurthalfkieke.repositories.BestellingRepository;
+import be.thomasmore.graduaten.frituurthalfkieke.repositories.StudentRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class BestellingController {
+
+    private BestellingRepository bestellingRepository;
+
+    public BestellingController(BestellingRepository studentRepository) {
+        this.bestellingRepository = studentRepository;
+    }
+
+    @RequestMapping("/bestelling")
+    public String navigateToBestelling(Model model) {
+        List<Bestelling> bestellingen = bestellingRepository.findAll();
+        model.addAttribute("bestellingen", bestellingen);
+        return "bestelling";
+    }
+
+    @RequestMapping("/bestelling/detail")
+    public String navigateToBestellingDetails(Model model, HttpServletRequest request) {
+        Long id = Long.parseLong(request.getParameter("id"));
+        Bestelling bestelling = bestellingRepository.getById(id);
+        model.addAttribute("bestelling", bestelling);
+        return "bestelling";
+    }
+
+    /*@RequestMapping("/bestelling/toevoegen")
+    public String navigateToAdStudentForm() {
+        return "bevestigingbestelling";
+    } */
+
+    //Form in winkelwagen of bevestiging bestelling????
+
+    @RequestMapping("/bestelling/toevoegen/result")
+    public String getResult(Model model, HttpServletRequest request) {
+        String voornaamKlant = request.getParameter("voornaam");
+        String familienaamKlant = request.getParameter("familienaam");
+        String emailKlant = request.getParameter("email");
+        String telefoonKlant = request.getParameter("telefoon");
+
+        Bestelling bestelling = new Bestelling(voornaamKlant, familienaamKlant, emailKlant,telefoonKlant);
+        bestellingRepository.save(bestelling);
+
+        List<Bestelling> bestellingen = bestellingRepository.findAll();
+        model.addAttribute("bestellingen", bestellingen);
+        return "bestelling";
+    }
+}
+
+
