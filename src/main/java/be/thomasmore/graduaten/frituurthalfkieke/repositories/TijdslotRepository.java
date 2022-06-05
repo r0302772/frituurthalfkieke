8e6dae33637fcddab67b60dd0bd994470db0928f
LoreasClonen <r072079@student.thomasmore.be>
2022-06-05 16:24:26 +0200

    fixed tijdsloten

--- src/main/java/be/thomasmore/graduaten/frituurthalfkieke/repositories/TijdslotRepository.java
+++ src/main/java/be/thomasmore/graduaten/frituurthalfkieke/repositories/TijdslotRepository.java
@@ -1,11 +1,11 @@
-package be.thomasmore.graduaten.frituurthalfkieke.repositories;
-
-import be.thomasmore.graduaten.frituurthalfkieke.entities.Tijdslot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-public interface TijdslotRepository  extends JpaRepository<Tijdslot,Long>  {
-
-}
+//package be.thomasmore.graduaten.frituurthalfkieke.repositories;
+//
+//import be.thomasmore.graduaten.frituurthalfkieke.entities.Tijdslot;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//public interface TijdslotRepository  extends JpaRepository<Tijdslot,Long>  {
+//
+//}
